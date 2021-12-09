46064f8720530842de686530807a8477f135ecf5
Riccardo Orion Feingold <riccardofeingold@gmail.com>
2021-12-09 21:24:09 +0100

    first code snippet to send one mail

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import smtplib
+import os
+from dotenv import load_dotenv
+
+# loading .env file
+load_dotenv()
+
+# accessing variables stored in .env
+my_email = os.environ.get("my-email")
+password = os.environ.get("password")
+recipient = os.environ.get("recipient")
+
+my_email = "riccardofeingold@gmail.com"
+
+connection = smtplib.SMTP("smtp.gmail.com") # connection to the mail server
+connection.starttls() # this makes the connection to the mail server secure
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs=recipient, msg="HELLO!")
+connection.close()
